9dd50f4d390f829fca261458d0a3d9510f3b419e
thomas-davignon <76546883+thomas-davignon@users.noreply.github.com>
2020-12-24 01:44:01 -0500

    Update xmaslights-spin.py
    Brownian motion on your Christmas tree !
    This should create a trail of lights moving around brownianly aroudn your Christmas tree (hopefully).
    Basically, it fades out the lights according to the time since last visit of a random walk, where every step we move with equal probability to any LED within a given radius (or the nearest LED if there aren't any LEDs within the radius).

--- xmaslights-spin.py
+++ xmaslights-spin.py
@@ -44,98 +44,104 @@ def xmaslight():
     
     
     # YOU CAN EDIT FROM HERE DOWN
+    # The Christmas Tree Brownian Motion !
+    import random
     
-    # I get a list of the heights which is not overly useful here other than to set the max and min altitudes
-    heights = []
-    for i in coords:
-        heights.append(i[2])
+    #   The way it works is this : it will visit a new light at every step, selected uniformly from
+    # the set of lights within a sphere of a given radius.
+    neighbourhood_radius = 50
     
-    min_alt = min(heights)
-    max_alt = max(heights)
+    # Here is a distance function to help us determine who are our neighbours.
+    def distance(p1, p2):
+        if len(p1) != len(p2) :
+            return -1
+        else :
+            dsq = 0
+            i = 0
+            while i < len(p1):
+                dsq += (p2[i]-p1[i]) ** 2
+            return dsq ** .5
     
-    # VARIOUS SETTINGS
+    #   The light where the walk IS will be a certain initial colour. It will then fade to black
+    # as time passes.
     
-    # how much the rotation points moves each time
-    dinc = 1
-    
-    # a buffer so it does not hit to extreme top or bottom of the tree
-    buffer = 200
-    
-    # pause between cycles (normally zero as it is already quite slow)
-    slow = 0
-    
-    # startin angle (in radians)
-    angle = 0
-    
-    # how much the angle changes per cycle
-    inc = 0.1
-    
-    # the two colours in GRB order
-    # if you are turning a lot of them on at once, keep their brightness down please
-    colourA = [0,50,50] # purple
-    colourB = [50,50,0] # yellow
-    
+    # This defines how the fade out happens over this period of time.
+    def color_fade_from_age(age):
+        # As a default, we start out bright white. But you could have it set up differently.
+        init_colour = [255, 255, 255]
+        
+        # This controls how long the fade out takes (in cycles)
+        fade_time = 500
+        
+        # These exponents control how the fade happens. The higher the exponent, the quicker that colour (RGB) fades out.
+        # The values here should make a fade out with more blue in it, but you can switch things up if you like !
+        exponent = [5, 5, 0.3]
+        f_age = age/fade_time
+        
+        if age < fade_time and age >= 0:
+            return [ int( init_colour[0] * (1-f_age) ** exponent[0] ), int( init_colour[1] * (1-f_age) ** exponent[1] ), int( init_colour[2] * (1-f_age) ** exponent[2] ) ]
+        else :
+            return [0,0,0]
     
     # INITIALISE SOME VALUES
+    current = 0 #index of the current LED
+    nearest = 0 #index of the nearest LED
+    neighbours = [] #list of indices of LEDs within the radius
+    ages = [-1] * len(coords) #ages (time since last visit) for all LEDs. -1 if never visited.
     
-    swap01 = 0
-    swap02 = 0
-    
-    # direct it move in
-    direction = -1
-    
-    # the starting point on the vertical axis
-    c = 100 
-    
-    # yes, I just run which run is true
-    run = 1
-    while run == 1:
+    # ... AND THEN FOREVER UNTIL THE END OF TIMES ...
+    while True:
         
         time.sleep(slow)
         
+        # On our first loop around, we figure out who our neighbours are.
+        # We also keep track of the nearest LED, just in case there's no one in our "neighbourhood".
+        
+        i = 0
+        nearest = (current + 1) % len(coords) # Just so we don't start with the current as nearest and never update it.
+        d_n = distance(coords[current], coords[nearest])
+        neighbours = [] 
+        
+        while i < len(coords):
+            if i != current:
+                d = distance(coords[current], coords[i]) #we get the distance to i.
+                
+                # Check if i is nearer than "nearest", and update accordingly.
+                if d < d_n:
+                    nearest = i
+                    d_n = d
+                    
+                # Check if we're within the radius and update neighbours if need be :
+                if d < neighbourhood_radius:
+                    neighbours.append(i)
+                    
+            i += 1
+            
+        # we should now have figured out who our neighbours are.
+        
+        # If we have no neighbours, we move to the nearest LED. Otherwise we just pick a neighbour at random.
+        if neighbours == []:
+            current = nearest
+        else:
+            current = random.choice(neighbours)
+        
+        # On our second loop around, we will set the colours for the LEDs in the current state, and
+        # update the ages for the next state :
         LED = 0
         while LED < len(coords):
-            if math.tan(angle)*coords[LED][1] <= coords[LED][2]+c:
-                pixels[LED] = colourA
-            else:
-                pixels[LED] = colourB
+            # Set the colour :
+            pixels[LED] = colour_fade_from_age(ages[LED])
+            
+            # update the ages
+            ages[LED] += 1
+            if LED == current:
+                ages[LED] = 0
+            
             LED += 1
         
         # use the show() option as rarely as possible as it takes ages
         # do not use show() each time you change a LED but rather wait until you have changed them all
         pixels.show()
-        
-        # now we get ready for the next cycle
-        
-        angle += inc
-        if angle > 2*math.pi:
-            angle -= 2*math.pi
-            swap01 = 0
-            swap02 = 0
-        
-        # this is all to keep track of which colour is 'on top'
-        
-        if angle >= 0.5*math.pi:
-            if swap01 == 0:
-                colour_hold = [i for i in colourA]
-                colourA =[i for i in colourB]
-                colourB = [i for i in colour_hold]
-                swap01 = 1
-                
-        if angle >= 1.5*math.pi:
-            if swap02 == 0:
-                colour_hold = [i for i in colourA]
-                colourA =[i for i in colourB]
-                colourB = [i for i in colour_hold]
-                swap02 = 1
-        
-        # and we move the rotation point
-        c += direction*dinc
-        
-        if c <= min_alt+buffer:
-            direction = 1
-        if c >= max_alt-buffer:
-            direction = -1
         
     return 'DONE'
 
